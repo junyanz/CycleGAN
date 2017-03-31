c7279bf61e8b8b5cacf4bf434ea67782017d935f
Travis Glines <travisglines@gmail.com>
2017-03-31 15:01:06 -0400

    Have wget not check certs as it fails

--- datasets/download_dataset.sh
+++ datasets/download_dataset.sh
@@ -8,7 +8,7 @@ fi
 URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $ZIP_FILE
+wget --no-check-certificate -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
